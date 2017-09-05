98ae944529e4187f247dd1a385da32d62832674c
Denzil Ferreira <ferreiradenzil@gmail.com>
2017-09-05 12:01:27 +0100

    - Fixed missing import.

--- com.aware.plugin.fitbit/src/main/java/com/aware/plugin/fitbit/FitbitOAuthServiceImpl.java
+++ com.aware.plugin.fitbit/src/main/java/com/aware/plugin/fitbit/FitbitOAuthServiceImpl.java
@@ -1,9 +1,9 @@
 package com.aware.plugin.fitbit;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.model.AbstractRequest;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.model.OAuthConfig;
+import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
 /**
@@ -17,7 +17,8 @@ class FitbitOAuthServiceImpl extends OAuth20Service {
     }
 
     @Override
-    public void signRequest(OAuth2AccessToken accessToken, AbstractRequest request) {
+    public void signRequest(OAuth2AccessToken accessToken, OAuthRequest request) {
         request.addHeader("Authorization", "Bearer " + accessToken.getAccessToken());
+        super.signRequest(accessToken, request);
     }
 }
